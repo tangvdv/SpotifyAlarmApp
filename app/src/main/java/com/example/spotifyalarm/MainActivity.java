16c4ba874bfe646bec7e89b0fc06ba56ac1cbe8a
TangVdv <tangrider77120@gmail.com>
2023-11-14 23:43:04 +0100

    Alarm button display + fix set alarm time
    - Reworked the global alarm button
    - Alarm now trigger the day after if  the time set is earlier than the actual time

--- app/src/main/java/com/example/spotifyalarm/MainActivity.java
+++ app/src/main/java/com/example/spotifyalarm/MainActivity.java
@@ -19,6 +19,7 @@ import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.CompoundButton;
 import android.widget.LinearLayout;
 import android.widget.Spinner;
 import android.widget.TextView;
@@ -46,11 +47,13 @@ import com.spotify.sdk.android.auth.AuthorizationResponse;
 import org.json.JSONObject;
 
 import java.io.Serializable;
+import java.security.Timestamp;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.List;
 import java.util.Objects;
+import java.util.TimeZone;
 
 public class MainActivity extends AppCompatActivity {
     public Context context;
@@ -66,15 +69,29 @@ public class MainActivity extends AppCompatActivity {
     private AlarmManager alarmManager;
     private PendingIntent pendingIntent;
 
+    private SharedPreferences sharedPreferences;
+
+    private int hour;
+    private int minute;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
+        sharedPreferences = this.getSharedPreferences("Settings", Context.MODE_PRIVATE);
+
         context = this;
 
         startActivity();
+
+        hour = sharedPreferences.getInt("time_hour", 0);
+        minute = sharedPreferences.getInt("time_minute", 0);
+
+        setCalendar();
+        setBtnSetTimeText();
+
 
         binding.btnSelect.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -83,27 +100,24 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        binding.btnAlarm.setOnClickListener(new View.OnClickListener() {
+        binding.btnSetTime.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 selectTime();
             }
         });
 
-        binding.btnSetAlarm.setOnClickListener(new View.OnClickListener() {
+        binding.setAlarmSwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             @Override
-            public void onClick(View view) {
-                setAlarm();
+            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+                if(b){
+                    setAlarm();
+                }
+                else{
+                    cancelAlarm();
+                }
             }
         });
-
-        binding.btnCancelAlarm.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                cancelAlarm();
-            }
-        });
-
     }
 
     public void startActivity(){
@@ -149,9 +163,6 @@ public class MainActivity extends AppCompatActivity {
     public void selectPlaylist(){
         PlaylistModel playlistModel = playlistModelList.get((int) binding.spinnerPlaylist.getSelectedItemId());
         playlist_uri = playlistModel.getSpotifyId();
-        SharedPreferences sharedPreferences = this.getSharedPreferences(
-                "Settings", Context.MODE_PRIVATE
-        );
         SharedPreferences.Editor editor = sharedPreferences.edit();
         editor.putString("playlist_uri", playlist_uri);
         editor.apply();
@@ -160,8 +171,8 @@ public class MainActivity extends AppCompatActivity {
     public void selectTime(){
         timePicker = new MaterialTimePicker.Builder()
                 .setTimeFormat(TimeFormat.CLOCK_24H)
-                .setHour(23)
-                .setMinute(0)
+                .setHour(sharedPreferences.getInt("time_hour", 0))
+                .setMinute(sharedPreferences.getInt("time_minute", 0))
                 .setTitleText("Select Alarm Time")
                 .build();
 
@@ -170,17 +181,33 @@ public class MainActivity extends AppCompatActivity {
         timePicker.addOnPositiveButtonClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                binding.btnAlarm.setText(
-                        timePicker.getHour() + "H" + timePicker.getMinute()
-                );
+                SharedPreferences.Editor editor = sharedPreferences.edit();
+                hour = timePicker.getHour();
+                minute = timePicker.getMinute();
+                editor.putInt("time_hour", timePicker.getHour());
+                editor.putInt("time_minute", timePicker.getMinute());
+                editor.apply();
 
-                calendar = calendar.getInstance();
-                calendar.set(Calendar.HOUR_OF_DAY, timePicker.getHour());
-                calendar.set(Calendar.MINUTE, timePicker.getMinute());
-                calendar.set(Calendar.SECOND, 0);
-                calendar.set(Calendar.MILLISECOND, 0);
+                setBtnSetTimeText();
+
+                setCalendar();
             }
         });
+    }
+
+    public void setBtnSetTimeText(){
+        binding.btnSetTime.setText(hour + ":" + minute);
+    }
+
+    public void setCalendar(){
+        calendar = calendar.getInstance();
+        calendar.set(Calendar.HOUR_OF_DAY, hour);
+        calendar.set(Calendar.MINUTE, minute);
+        calendar.set(Calendar.SECOND, 0);
+        calendar.set(Calendar.MILLISECOND, 0);
+        if(calendar.getTimeInMillis() < System.currentTimeMillis()){
+            calendar.add(Calendar.DATE, 1);
+        }
     }
 
     public void setAlarm(){
@@ -193,19 +220,19 @@ public class MainActivity extends AppCompatActivity {
         createText(
                 new SimpleDateFormat("HH:mm:ss").format(calendar.getTime()) + " ; " + new SimpleDateFormat("HH:mm:ss:SSS").format(Calendar.getInstance().getTime().getTime())
         );
+        createText(calendar.getTimeInMillis() + " ; " + System.currentTimeMillis());
 
         Toast.makeText(this, "Alarm Set", Toast.LENGTH_SHORT).show();
     }
 
     public void cancelAlarm(){
-        Intent intent = new Intent(this, AlarmReceiver.class);
-        pendingIntent = PendingIntent.getBroadcast(this, 0, intent, PendingIntent.FLAG_IMMUTABLE);
-
-        if(alarmManager == null){
-            alarmManager = (AlarmManager) getSystemService(Context.ALARM_SERVICE);
+        if(pendingIntent != null){
+            if(alarmManager == null){
+                alarmManager = (AlarmManager) getSystemService(Context.ALARM_SERVICE);
+            }
+            alarmManager.cancel(pendingIntent);
+            Toast.makeText(this, "Alarm Canceled", Toast.LENGTH_SHORT).show();
         }
-        alarmManager.cancel(pendingIntent);
-        Toast.makeText(this, "Alarm Canceled", Toast.LENGTH_SHORT).show();
     }
 
     public void createText(String text){
