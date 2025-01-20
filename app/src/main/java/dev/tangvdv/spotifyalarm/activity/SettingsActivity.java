dec5fe86595f0ff4c4c95af2fcc60c1e54fcf043
TangVdv <tangrider77120@gmail.com>
2025-01-20 18:59:30 +0100

    Added ringtone test sound on volume settings change

--- app/src/main/java/dev/tangvdv/spotifyalarm/activity/SettingsActivity.java
+++ app/src/main/java/dev/tangvdv/spotifyalarm/activity/SettingsActivity.java
@@ -1,7 +1,12 @@
 package dev.tangvdv.spotifyalarm.activity;
 
 import android.content.Context;
+import android.media.AudioAttributes;
 import android.media.AudioManager;
+import android.media.Ringtone;
+import android.media.RingtoneManager;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.CompoundButton;
@@ -19,6 +24,8 @@ public class SettingsActivity extends AppCompatActivity {
     private Context context;
     private SettingsActivityBinding binding;
     private SettingsModel settingsModel;
+    private Ringtone defaultRingtone;
+    private AudioManager am;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -27,6 +34,7 @@ public class SettingsActivity extends AppCompatActivity {
         binding = SettingsActivityBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
         settingsModel = new SettingsModel(AlarmSharedPreferences.loadSettings(context));
+        am = (AudioManager) getSystemService(Context.AUDIO_SERVICE);
 
         bindingManager();
         setSettingsView();
@@ -48,6 +56,7 @@ public class SettingsActivity extends AppCompatActivity {
         binding.btnCancelSettings.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                stopRingtone();
                 finish();
             }
         });
@@ -55,6 +64,7 @@ public class SettingsActivity extends AppCompatActivity {
         binding.btnApplySettings.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                stopRingtone();
                 applySettings();
             }
         });
@@ -65,6 +75,7 @@ public class SettingsActivity extends AppCompatActivity {
 
                 settingsModel.setRepeat(!settingsModel.isRepeat());
                 binding.switchRepeatSettings.setChecked(settingsModel.isRepeat());
+                stopRingtone();
             }
         });
 
@@ -72,6 +83,7 @@ public class SettingsActivity extends AppCompatActivity {
             @Override
             public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
                 settingsModel.setRepeat(b);
+                stopRingtone();
             }
         });
 
@@ -80,6 +92,7 @@ public class SettingsActivity extends AppCompatActivity {
             public void onClick(View view) {
                 settingsModel.setShuffle(!settingsModel.isShuffle());
                 binding.switchShuffleSettings.setChecked(settingsModel.isShuffle());
+                stopRingtone();
             }
         });
 
@@ -88,6 +101,7 @@ public class SettingsActivity extends AppCompatActivity {
             public void onClick(View view) {
                 settingsModel.setLoopMusic(!settingsModel.isLooping());
                 binding.switchLoopSettings.setChecked(settingsModel.isLooping());
+                stopRingtone();
             }
         });
 
@@ -95,6 +109,7 @@ public class SettingsActivity extends AppCompatActivity {
             @Override
             public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
                 settingsModel.setShuffle(b);
+                stopRingtone();
             }
         });
 
@@ -107,6 +122,7 @@ public class SettingsActivity extends AppCompatActivity {
                     binding.seekBarSoundVolume.setProgress(min);
                 }
                 settingsModel.setVolume(i);
+                playRingtone(i);
             }
 
             @Override
@@ -124,6 +140,7 @@ public class SettingsActivity extends AppCompatActivity {
             @Override
             public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
                 settingsModel.setLoopMusic(b);
+                stopRingtone();
             }
         });
 
@@ -132,5 +149,34 @@ public class SettingsActivity extends AppCompatActivity {
     private void applySettings(){
         AlarmSharedPreferences.saveSettings(context, settingsModel.getSettingsModelContent());
         finish();
+    }
+
+    private void getRingtone(){
+        Uri defaultRingtoneUri = RingtoneManager.getActualDefaultRingtoneUri(getApplicationContext(), RingtoneManager.TYPE_ALARM);
+        defaultRingtone = RingtoneManager.getRingtone(this, defaultRingtoneUri);
+
+        AudioAttributes audioAttributes = new AudioAttributes.Builder()
+                .setUsage(AudioAttributes.USAGE_ALARM)
+                .setContentType(AudioAttributes.CONTENT_TYPE_MUSIC)
+                .build();
+
+        defaultRingtone.setAudioAttributes(audioAttributes);
+    }
+
+    private void playRingtone(int i){
+        if(defaultRingtone == null) getRingtone();
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P) {
+            float volume = (float) i / am.getStreamMaxVolume(AudioManager.STREAM_ALARM);
+            defaultRingtone.setVolume(volume);
+        }
+
+        if(!defaultRingtone.isPlaying()) defaultRingtone.play();
+    }
+
+    private void stopRingtone(){
+        if(defaultRingtone == null) getRingtone();
+
+        if(defaultRingtone.isPlaying()) defaultRingtone.stop();
     }
 }
